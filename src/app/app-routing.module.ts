d38a8213982abd54eefdf263e48213f7ffb8ca7a
Gastón Camú <camugaston2002@gmail.com>
2023-09-05 14:50:32 -0300

    Se agrego un nuevo componente y su enlace

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { NewCompComponent } from './new-comp/new-comp.component';
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path:'new-comp',
+    component: NewCompComponent,
   }
 ];
 @NgModule({
