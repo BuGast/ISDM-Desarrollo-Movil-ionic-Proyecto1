c6afbf87f653d86e7a6cf645b90363569f8eeb73
Gastón Camú <camugaston2002@gmail.com>
2023-09-27 15:03:43 -0300

    se realizaron nuevos cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,8 +15,36 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
+    path: 'salir',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
     path:'formulario',
     loadChildren: () => import('./formulario/formulario.module').then( m => m.FormularioPageModule)
+  },
+  {
+    path: 'perro',
+    loadChildren: () => import('./pages/perro/perro.module').then( m => m.PerroPageModule)
+  },
+  {
+    path: 'armadura',
+    loadChildren: () => import('./pages/armadura/armadura.module').then( m => m.ArmaduraPageModule)
+  },
+  {
+    path: 'pizarron',
+    loadChildren: () => import('./pages/pizarron/pizarron.module').then( m => m.PizarronPageModule)
+  },
+  {
+    path: 'piedra',
+    loadChildren: () => import('./pages/piedra/piedra.module').then( m => m.PiedraPageModule)
+  },
+  {
+    path: 'computadora',
+    loadChildren: () => import('./pages/computadora/computadora.module').then( m => m.ComputadoraPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   }
 ];
 @NgModule({
