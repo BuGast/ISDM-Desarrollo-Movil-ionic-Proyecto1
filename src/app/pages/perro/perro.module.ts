f312642df2f1ca2a872438421e3c7eb8f7f95f99
Gastón Camú <camugaston2002@gmail.com>
2023-09-27 16:03:10 -0300

    se agrego componentes y add img

--- src/app/pages/perro/perro.module.ts
+++ src/app/pages/perro/perro.module.ts
@@ -7,13 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { PerroPageRoutingModule } from './perro-routing.module';
 
 import { PerroPage } from './perro.page';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PerroPageRoutingModule
+    PerroPageRoutingModule,
+    ComponentsModule,
   ],
   declarations: [PerroPage]
 })
